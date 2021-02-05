a5809eb0e816af643738fdc6f9d0ab672034aa2f
Lee Eun Deok <pleed0215@gmail.com>
2021-02-05 19:56:13 +0900

    done

--- src/pages/listener/__test__/episodes.spec.tsx
+++ src/pages/listener/__test__/episodes.spec.tsx
@@ -1,24 +1,127 @@
 import { render, waitFor } from "../../../test-utils";
-import React from "react";
-import { Episodes } from "../episodes";
-import { ApolloProvider } from "@apollo/client";
+import React, { ReactElement } from "react";
+import {
+  ApolloClient,
+  ApolloProvider,
+  from,
+  InMemoryCache,
+} from "@apollo/client";
 import { RenderResult } from "@testing-library/react";
 import { createMockClient, MockApolloClient } from "mock-apollo-client";
-import * as reactRouterDom from "react-router-dom";
+import { Episodes, GET_EPISODES_QUERY } from "../episodes";
+import { Route, Router } from "react-router-dom";
+import { createMemoryHistory } from "history";
+import { MockedProvider } from "@apollo/client/testing";
+import { act } from "react-dom/test-utils";
+import {
+  getEpisodes,
+  getEpisodesVariables,
+} from "../../../__type_graphql__/getEpisodes";
 
-describe("<Episodes />", () => {
+const podcastData = {
+  getPodcast: {
+    ok: true,
+    podcast: {
+      id: 1,
+      title: "test",
+      category: "test",
+      thumbnailUrl: "test",
+      description: "test",
+      rating: 1,
+    },
+  },
+};
+
+const episodeData = {
+  getEpisodes: {
+    ok: true,
+    episodes: [
+      {
+        title: "test",
+        description: "test",
+      },
+    ],
+  },
+};
+
+const resolveData: getEpisodes = {
+  getPodcast: {
+    __typename: "PodcastOutput",
+    ok: true,
+    error: null,
+    podcast: {
+      __typename: "Podcast",
+      id: 1,
+      title: "test title",
+      category: "test category",
+      thumbnailUrl: "test url",
+      description: "test description",
+      rating: 1,
+    },
+  },
+  getEpisodes: {
+    __typename: "EpisodesOutput",
+    ok: true,
+    error: null,
+    episodes: [
+      {
+        __typename: "Podcast",
+        title: "test episode title",
+        description: "test episode description",
+      },
+    ],
+  },
+};
+
+const mocks = [
+  {
+    request: {
+      query: GET_EPISODES_QUERY,
+      variables: {
+        input: {
+          id: 1,
+        },
+      },
+    },
+    result: () => {
+      console.log(resolveData);
+      return {
+        data: resolveData,
+      };
+    },
+  },
+];
+
+describe("<Podcasts />", () => {
   let mockedClient: MockApolloClient;
   let renderResult: RenderResult;
+  let mockedQueryResolve: jest.Mock;
 
   beforeEach(async () => {
     await waitFor(() => {
+      const history = createMemoryHistory({
+        initialEntries: ["/episodes/1"],
+      });
+      mockedQueryResolve = jest.fn().mockResolvedValue({ data: resolveData });
       mockedClient = createMockClient();
+      mockedClient.setRequestHandler(GET_EPISODES_QUERY, mockedQueryResolve);
       renderResult = render(
         <ApolloProvider client={mockedClient}>
-          <Episodes />
+          <Router history={history}>
+            <Route path="/episodes/:id">
+              <Episodes />
+            </Route>
+          </Router>
         </ApolloProvider>
       );
     });
   });
-  it("renders OK", async () => {});
+  it("renders OK", async () => {
+    await act(async () => {
+      await new Promise((resolve) => setTimeout(resolve, 0));
+    });
+
+    const { debug } = renderResult;
+    expect(mockedQueryResolve).toHaveBeenCalledTimes(1);
+  });
 });
--- src/pages/listener/__test__/podcasts.spec.tsx
+++ src/pages/listener/__test__/podcasts.spec.tsx
@@ -1,16 +1,39 @@
 import { render, waitFor } from "../../../test-utils";
 import React from "react";
 import { ApolloProvider } from "@apollo/client";
-import { RenderResult } from "@testing-library/react";
+import { act, RenderResult } from "@testing-library/react";
 import { createMockClient, MockApolloClient } from "mock-apollo-client";
-import { Podcasts } from "../podcasts";
+import { ALLPODCASTS_QUERY, Podcasts } from "../podcasts";
+import { getAllPodcastQuery } from "../../../__type_graphql__/getAllPodcastQuery";
+
+const resolveData: getAllPodcastQuery = {
+  getAllPodcasts: {
+    __typename: "GetAllPodcastsOutput",
+    ok: true,
+    error: null,
+    podcasts: [
+      {
+        __typename: "Podcast",
+        id: 1,
+        title: "test title",
+        category: "Test category",
+        thumbnailUrl: "testurl.jpg",
+        description: "test description",
+        rating: 1,
+      },
+    ],
+  },
+};
 
 describe("<Podcasts />", () => {
   let mockedClient: MockApolloClient;
   let renderResult: RenderResult;
+  let mockedQueryResponse: jest.Mock;
   beforeEach(async () => {
     await waitFor(() => {
+      mockedQueryResponse = jest.fn().mockResolvedValue({ data: resolveData });
       mockedClient = createMockClient();
+      mockedClient.setRequestHandler(ALLPODCASTS_QUERY, mockedQueryResponse);
       renderResult = render(
         <ApolloProvider client={mockedClient}>
           <Podcasts />
@@ -18,5 +41,10 @@ describe("<Podcasts />", () => {
       );
     });
   });
-  it("renders OK", async () => {});
+  it("renders OK", async () => {
+    await act(async () => {
+      await new Promise((resolve) => setTimeout(resolve, 0));
+    });
+    expect(mockedQueryResponse).toHaveBeenCalledTimes(1);
+  });
 });
